abd11d43a94e2e46fdd9c01c434087176f4e35a1
amir-pazhuhan <amir.pazhuhan95@proton.me>
2026-05-23 09:50:09 +0330

    Added model and views code for orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .models import Product
 # Create your models here.
+
+class Order(models.Model):
+    choices = [
+        ('pending', 'Pending'),
+        ('shipped', 'Shipped'),
+        ('paid', 'Paid')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=choices, default='pending')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveBigIntegerField()
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView,Response
+from rest_framework.exceptions import ValidationError
+from django.db import transaction
+from .models import Cart, Order, OrderItem
 # Create your views here.
+
+class CreateOrder(APIView):
+
+    def post(self, request):
+
+
+        try:
+            with transaction.atomic():
+                cart = Cart.objects.get(user=request.user)
+                cart_items = cart.items.select_for_update().all()
+                
+                if not cart_items.exists():
+                    return Response({"error": "Cart is empty"}, status=400)
+                
+                order = Order.objects.create(user= request.user, total_price=0)
+
+                total=0
+
+                for item in cart_items:
+                    price = item.item.price
+
+                    if item.item.inventory < item.quantity:
+                        raise ValidationError(f"Not enough stock for {item.item.name}")
+                    
+                    item.item.inventory -= item.quantity
+                    item.item.save()
+
+                    order_item = OrderItem.objects.create(
+                        order=order,
+                        price_at_purchase=price,
+                        product=item.item,
+                        quantity= item.quantity,
+                    )
+                    total += price * item.quantity
+
+                order.total_price= total
+                order.save()
+                cart.items.all().delete()
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
